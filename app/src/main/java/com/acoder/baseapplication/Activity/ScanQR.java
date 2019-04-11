52ee888e6c64bcc8dcbc4cd40a9a4c6fbcad6409
SHAKI <spider.shaki@gmail.com>
2019-04-12 02:47:09 +0600

    baseApplication

---
+++ app/src/main/java/com/acoder/baseapplication/Activity/ScanQR.java
@@ -0,0 +1,15 @@
+package com.acoder.baseapplication.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.acoder.baseapplication.R;
+
+public class ScanQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+    }
+}
