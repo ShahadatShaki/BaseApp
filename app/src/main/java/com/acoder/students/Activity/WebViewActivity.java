ba78a7cde7dbb8f37a136a9e6a885a0d8491fe3b
Shahadat Mollah <shahadat@maxproit.solutions>
2020-01-23 11:26:23 +0600

    webview activity

---
+++ app/src/main/java/com/acoder/students/Activity/WebViewActivity.java
@@ -0,0 +1,34 @@
+package com.acoder.students.Activity;
+
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+
+import com.acoder.students.Base.BaseActivity;
+import com.acoder.students.R;
+import com.acoder.students.databinding.ActivityWebViewBinding;
+
+public class WebViewActivity extends BaseActivity {
+
+    private ActivityWebViewBinding binding;
+
+    @Override
+    protected int getLayoutResourceFile() {
+
+        return R.layout.activity_web_view;
+    }
+
+    @Override
+    protected void initComponent() {
+
+        binding = (ActivityWebViewBinding) getBinding();
+        String title = getIntent().getStringExtra("title");
+        String url = getIntent().getStringExtra("url");
+
+        setToolbar(title);
+        //Toast.makeText(this, url, Toast.LENGTH_SHORT).show();
+
+        binding.webView.loadUrl(url);
+    }
+
+}
