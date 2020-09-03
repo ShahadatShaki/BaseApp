9c1d7a32236f3b792b3839f31adff01bbb572a4f
Shaki <spider.shaki@gmail.com>
2020-09-03 15:30:26 +0600

    new functions and layout added

--- app/src/main/java/com/acoder/students/Activity/HomePage.java
+++ app/src/main/java/com/acoder/students/Activity/HomePage.java
@@ -1,12 +1,17 @@
 package com.acoder.students.Activity;
 
 import android.content.Context;
+import android.view.View;
 
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.acoder.students.Adapter.CommonAdapter;
 import com.acoder.students.Base.BaseActivity;
 
 import com.acoder.students.R;
+import com.acoder.students.Utility.EndlessRecyclerViewScrollListener;
 import com.acoder.students.ViewModel.UserControlViewModel;
 import com.acoder.students.databinding.ActivityHomePageBinding;
 
@@ -17,7 +22,7 @@ public class HomePage extends BaseActivity {
     ActivityHomePageBinding b;
     Context context;
     UserControlViewModel viewModel;
-
+    private int totalPage;
 
 
     @Override
@@ -44,7 +49,6 @@ public class HomePage extends BaseActivity {
         });
     }
 
-
     private void populateList(ArrayList<Object> merchantProfiles) {
 
 //        b.recycleView.setVisibility(View.VISIBLE);
@@ -55,5 +59,42 @@ public class HomePage extends BaseActivity {
 //        b.recycleView.setAdapter(adapter);
 
     }
+
+    private void initRecycleView() {
+
+        ArrayList<String > mcqModels = new ArrayList<>();
+        b.recycleView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
+        b.recycleView.setLayoutManager(linearLayoutManager);
+
+        CommonAdapter adapter = new CommonAdapter(context, mcqModels);
+        b.recycleView.setAdapter(adapter);
+
+        showProgressDialog("Getting Questions..");
+//        getMcqQuestions("1");
+
+        EndlessRecyclerViewScrollListener scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                page += 1;
+                if (page <= totalPage) {
+//                    getMcqQuestions(String.valueOf(page));
+//                    b.progressBar.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onScroll(int dx, int dy) {
+
+            }
+
+        };
+
+        b.recycleView.addOnScrollListener(scrollListener);
+        // Adds the scroll listener to RecyclerView
+
+
+    }
+
 
 }
---
+++ app/src/main/java/com/acoder/students/Utility/ItemClick.java
@@ -0,0 +1,5 @@
+package com.acoder.students.Utility;
+
+public interface ItemClick {
+    public void onItemClick(int position, int viewId);
+}
